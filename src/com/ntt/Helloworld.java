95d7fe568483b4178f11ffe15811dce87048e735
133237 <133237@INBLRWHF3HKT6C2.AMERICAS.GLOBAL.NTTDATA.COM>
2019-11-15 15:20:23 +0530

    print helloworld message as sin wave

--- src/com/ntt/Helloworld.java
+++ src/com/ntt/Helloworld.java
@@ -1,9 +1,41 @@
 package com.ntt;
 
+import java.util.Random;
+
 public class Helloworld {
 
-	public static void main(String[] args) {
-		System.out.println("hello world");
+  static char[] cArr = "Hello world   ".toCharArray();
+  static int index = -1;
+  static String[] arr = {"\u001B[91m","\u001B[92m","\u001B[93m","\u001B[94m","\u001B[95m","\u001B[96m"};
+  static int colorNumber = 1;
+
+  public static void main(String[] args) throws InterruptedException  {
+    printHellowrld();
+  }
+  
+  public  static void printHellowrld() throws InterruptedException {
+	  for (double x = 0; x <= Math.PI; x += 0.15) {
+			int sinValue = (int) Math.round(Math.sin(x) * 60);
+			for (int j = 0; j < sinValue; j++) {
+
+				System.out.print(arr[colorNumber] + "" + nextChar());
+			}
+			Thread.sleep(5);
+			System.out.println();
+		} 
+  }
+
+  public static char nextChar() {
+	index++;
+	if (index == 12) {
+		Random r = new Random();
+		colorNumber = r.nextInt(arr.length);
+	}
+	if (index > 13) {
+		index = 0;
+
 	}
 
+	return cArr[index];
+  }
 }
